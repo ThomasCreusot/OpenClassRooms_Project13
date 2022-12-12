ee9a3799804b8b87d3c19c54fa52f9ce41ac6efc
Thomas Creusot <th.creusot@gmail.com>
2022-12-12 14:51:35 +0100

    initial commit ; pip install six and pip freeze > requirements.txt

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\thcre\Documents\04_Developpement informatique\01_PROJETS_OCR\Projet_13\P13_OC\OpenClassRooms_Project13\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
