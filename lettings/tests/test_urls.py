ac35c6a8032551a0da678fb09c47e48e74631370
Thomas Creusot <th.creusot@gmail.com>
2022-12-19 15:47:58 +0100

    redaction of test_letting_detail_url in  lettings/tests/test_urls.py

--- lettings/tests/test_urls.py
+++ lettings/tests/test_urls.py
@@ -30,3 +30,29 @@ def test_letting_list_url():
     path_index = reverse('author-lettings:lettings_index', current_app="author-lettings")
     assert path_index == "/author-lettings/lettings/"  # checks if URL is right
     assert resolve(path_index).view_name == "author-lettings:lettings_index"  # checks if view name is right
+
+
+@pytest.mark.django_db
+def test_letting_detail_url():
+    """Tests the URL and view name of the page displaying letting objects as in details"""
+
+    address_object_for_testing = Address.objects.create(
+        number = 1,
+        street = "street_of_testing",
+        city = "city_of_testing",
+        state = "NY",
+        zip_code = 11554,
+        country_iso_code = "USA"
+    )
+
+    letting_object_for_testing = Letting.objects.create(
+        title = "letting_object_for_testing",
+        address = address_object_for_testing
+    )
+
+    # author-lettings : namespace in oc_lettings_site/urls.py
+    # letting: from lettings.views import letting
+    # letting_id is from: path('lettings/<int:letting_id>/', letting, name='letting')
+    path_letting = reverse('author-lettings:letting', current_app="author-lettings", kwargs={'letting_id':1})
+    assert path_letting == "/author-lettings/lettings/1/"  # checks if URL is right
+    assert resolve(path_letting).view_name == "author-lettings:letting"  # checks if view name is right
