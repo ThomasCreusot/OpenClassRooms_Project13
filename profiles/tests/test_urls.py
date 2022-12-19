2012f4805f9b85867500a898a2d60ae1cf475217
Thomas Creusot <th.creusot@gmail.com>
2022-12-19 15:58:09 +0100

    redaction of test_profile_list_url in  profiles/tests/test_urls.py

--- profiles/tests/test_urls.py
+++ profiles/tests/test_urls.py
@@ -1,1 +1,31 @@
-#profiles
+import pytest
+
+from django.contrib.auth.models import User
+
+from django.urls import reverse, resolve
+from django.test import Client
+
+from profiles.models import Profile
+
+
+@pytest.mark.django_db
+def test_profile_list_url():
+    """Tests the URL and view name of the page displaying profile objects as a list"""
+
+    user_object_for_testing = User.objects.create(
+        username = "user_username_for_testing",
+        password = "user_password_for_testing"
+    )
+
+    profile_object_for_testing = Profile.objects.create(
+        user = user_object_for_testing,
+        favorite_city = "favorite_city_for_testing"
+    )
+
+    # Generates the URL from the view name
+    # author-profile: namespace in oc_lettings_site/urls.py
+    # profiles_index: see profiles/urls.py: from profiles.views import index as profiles_index
+    path_index = reverse('author-profiles:profiles_index', current_app="author-profiles")
+    assert path_index == "/author-profiles/profiles/"  # checks if URL is right
+    assert resolve(path_index).view_name == "author-profiles:profiles_index"  # checks if view name is right
+
