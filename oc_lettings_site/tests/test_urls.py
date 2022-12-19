7765d253191ac87cd251cbb25f1391df8c423bfd
Thomas Creusot <th.creusot@gmail.com>
2022-12-19 16:27:34 +0100

    redaction of test_index_view in oc_lettings_site/tests/test_urls.py

--- oc_lettings_site/tests/test_urls.py
+++ oc_lettings_site/tests/test_urls.py
@@ -1,5 +1,3 @@
-import pytest
-
 from django.urls import reverse, resolve
 from django.test import Client
 
@@ -13,3 +11,20 @@ def test_index_url():
     path_index = reverse('index')
     assert path_index == "/"  # checks if URL is right
     assert resolve(path_index).view_name == "index"  # checks if view name is right
+
+
+def test_index_view():
+    """Tests the view content of the index page.
+    Specifications : as each website page contains a title, each test must verify it in the
+    HTML answer. Response status code il also tested."""
+
+    client = Client()  # Equivalent to a simplified web browser
+
+    path_index = reverse('index')
+    response = client.get(path_index)
+    content = response.content.decode()
+    print(content)
+    expected_content = "<h1>Welcome to Holiday Homes</h1>"
+
+    assert content.find(expected_content) != -1
+    assert response.status_code == 200
