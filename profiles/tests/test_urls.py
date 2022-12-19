891bf8c0b57f5e10a4dd639cb84a5140476877ff
Thomas Creusot <th.creusot@gmail.com>
2022-12-19 16:03:59 +0100

    test_profile_detail_url

--- profiles/tests/test_urls.py
+++ profiles/tests/test_urls.py
@@ -29,3 +29,25 @@ def test_profile_list_url():
     assert path_index == "/author-profiles/profiles/"  # checks if URL is right
     assert resolve(path_index).view_name == "author-profiles:profiles_index"  # checks if view name is right
 
+
+@pytest.mark.django_db
+def test_profile_detail_url():
+    """Tests the URL and view name of the page displaying profile objects in details"""
+
+    user_object_for_testing = User.objects.create(
+        username = "user_username_for_testing",
+        password = "user_password_for_testing"
+    )
+
+    profile_object_for_testing = Profile.objects.create(
+        user = user_object_for_testing,
+        favorite_city = "favorite_city_for_testing"
+    )
+
+    # author-profiles : namespace in oc_lettings_site/urls.py
+    # profiles is from : in urls.py : from profiles.views import profile
+    # username is from: path('profiles/<str:username>/', profile, name='profile'),
+    path_profile = reverse('author-profiles:profile', current_app="author-profiles", kwargs={'username':1})
+    assert path_profile == "/author-profiles/profiles/1/"  # checks if URL is right
+    assert resolve(path_profile).view_name == "author-profiles:profile"  # checks if view name is right
+
