8da2b9cf82ef2aaa4292fe11e7c796ce5451b526
Thomas Creusot <th.creusot@gmail.com>
2022-12-19 15:48:28 +0100

    redaction of test_letting_list_view in  lettings/tests/test_urls.py

--- lettings/tests/test_urls.py
+++ lettings/tests/test_urls.py
@@ -56,3 +56,32 @@ def test_letting_detail_url():
     path_letting = reverse('author-lettings:letting', current_app="author-lettings", kwargs={'letting_id':1})
     assert path_letting == "/author-lettings/lettings/1/"  # checks if URL is right
     assert resolve(path_letting).view_name == "author-lettings:letting"  # checks if view name is right
+
+
+@pytest.mark.django_db  
+def test_letting_list_view():
+    """Tests the view content of the page displaying letting objects in details.
+    Specifications : as each website page contains a title, each test must verify it in the
+    HTML answer. Response status code il also tested."""
+
+    client = Client()  # Equivalent to a simplified web browser
+    address_object_for_testing = Address.objects.create(
+        number = 1,
+        street = "street_of_testing",
+        city = "city_of_testing",
+        state = "NY",
+        zip_code = 11554,
+        country_iso_code = "USA"
+    )
+    letting_object_for_testing = Letting.objects.create(
+        title = "letting_object_for_testing",
+        address = address_object_for_testing
+    )
+
+    path_index = reverse('author-lettings:lettings_index', current_app="author-lettings")
+    response = client.get(path_index)
+    content = response.content.decode()
+    expected_content = "<h1>Lettings</h1>"
+
+    assert content.find(expected_content) != -1
+    assert response.status_code == 200
