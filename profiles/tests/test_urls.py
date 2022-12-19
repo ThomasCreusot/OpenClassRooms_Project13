696b7f4bda3748b4a5f2589bb65d20d43e228c6c
Thomas Creusot <th.creusot@gmail.com>
2022-12-19 16:06:06 +0100

    redaction of test_profile_list_view  in  profiles/tests/test_urls.py

--- profiles/tests/test_urls.py
+++ profiles/tests/test_urls.py
@@ -51,3 +51,28 @@ def test_profile_detail_url():
     assert path_profile == "/author-profiles/profiles/1/"  # checks if URL is right
     assert resolve(path_profile).view_name == "author-profiles:profile"  # checks if view name is right
 
+
+@pytest.mark.django_db  
+def test_profile_list_view():
+    """Tests the view content of the page displaying profile objects in details.
+    Specifications : as each website page contains a title, each test must verify it in the
+    HTML answer. Response status code il also tested."""
+
+    client = Client()  # Equivalent to a simplified web browser
+    user_object_for_testing = User.objects.create(
+        username = "user_username_for_testing",
+        password = "user_password_for_testing"
+    )
+
+    profile_object_for_testing = Profile.objects.create(
+        user = user_object_for_testing,
+        favorite_city = "favorite_city_for_testing"
+    )
+
+    path_index = reverse('author-profiles:profiles_index', current_app="author-profiles")
+    response = client.get(path_index)
+    content = response.content.decode()
+    expected_content = "<h1>Profiles</h1>"
+
+    assert content.find(expected_content) != -1
+    assert response.status_code == 200
