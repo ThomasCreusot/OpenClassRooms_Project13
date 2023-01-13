6d0e3808c6e6abbb31a44f217c19b2ca76d93e25
Thomas Creusot <th.creusot@gmail.com>
2023-01-13 16:17:05 +0100

    testing deployment with secret_key and sentry_dsn keys: testing to collectstatic with os.environ.get instead of os.environ[]

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-#SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
